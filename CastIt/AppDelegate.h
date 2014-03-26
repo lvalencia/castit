57343ef774c06216ac28bd3bec3d728d0d00fa24
lvalencia <luis.valencia@basisworldwide.com>
2014-03-25 21:26:09 -0700

    Initial Commit

---
+++ CastIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CastIt
+//
+//  Created by Luis Valencia on 3/25/14.
+//  Copyright (c) 2014 Luis Valencia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
